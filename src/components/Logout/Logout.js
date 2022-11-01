16fc69070586ef5a78d8616953bf76a415fba84b
ghost32 <kalash.strt@gmail.com>
2022-11-01 22:06:46 +0530

    fix: navigation after logout

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,5 +1,6 @@
 // importing components
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 // importing context
 import AuthContext from '../../context/auth';
@@ -7,9 +8,11 @@ import AuthContext from '../../context/auth';
 // logout function
 function Logout() {
     const authCtx = useContext(AuthContext);
+    const navigator = useNavigate();
 
     const handleLogout = () => {
         authCtx.logout();
+        navigator('/', { replace: true })
     }
 
     return (
