c29c1138abb60321c2a889e28f1f139d8b48480e
ghost32 <kalash.strt@gmail.com>
2022-11-01 10:50:59 +0530

    feat($navbar): navbar is created
     - navbar is created in support of current layout
     - routes are added for navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,38 @@
+// importing components
+import { Link } from 'react-router-dom';
+
 // navbar component
 function Navbar() {
     return (
-        <div>
-            Navbar Component
-        </div>
+        <nav>
+            <div>
+                Pet Station
+            </div>
+            <ul>
+                <li>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/login">
+                        Login
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/signup">
+                        Signup
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/dashboard">
+                        Dashboard
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
