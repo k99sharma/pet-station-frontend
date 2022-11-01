1d8fac67cc8b464b88469e807e8eea3d52e08501
ghost32 <kalash.strt@gmail.com>
2022-11-01 11:40:22 +0530

    add: login form is needed to be created

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,64 @@
-// signup components
+// importing components
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import { useEffect, useState } from 'react';
+
+// login components
 function Login() {
+    const [credentials, setCredentials] = useState(null);
+
+    // need to login user
+    useEffect(() => {
+        console.table(credentials);
+    }, [credentials])
+
     return (
-        <div className="signup">
-            Login Page
-        </div>
+        <div className="login container">
+            <h1>
+                Login Page
+            </h1>
+
+            <Formik
+                initialValues={{ email: '', password: '' }}
+
+                validate={values => {
+                    // TODO: custom validation needed here
+
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = 'Required';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                }}
+
+                onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                        actions.setSubmitting(false);
+                    }, 400);
+
+                    // setting data into our states
+                    setCredentials(values);
+
+                    // reset the form
+                    actions.resetForm();
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form className="loginForm">
+                        <Field type="email" name="email" placeholder="Email Address" />
+                        <ErrorMessage name="email" component="div" />
+                        <Field type="password" name="password" placeholder="Password" />
+                        <ErrorMessage name="password" component="div" />
+                        <button type="submit" disabled={isSubmitting}>
+                            Login
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div >
     )
 }
 
