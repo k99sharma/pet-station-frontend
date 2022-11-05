6b9cd69cf31f8106c7efa6ed3590a4a4b09bc20c
ghost32 <kalash.strt@gmail.com>
2022-11-06 00:45:50 +0530

    ref: pages are cleared
     all pages are cleared for fresh start of v1

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,26 +1,8 @@
-// importing components
-import { useContext } from 'react';
-
-// importing components
-import UserDashboard from '../../components/UserDashboard/UserDashboard';
-import AdminDashboard from '../../components/AdminDashboard/AdminDashboard';
-
-// importing context
-import AuthContext from '../../context/auth';
-
 // dashboard function 
 function Dashboard() {
-    const authCtx = useContext(AuthContext);
-
     return (
         <div className="dashboard">
-            {
-                authCtx.user.role === 'admin'
-                    ?
-                    <AdminDashboard />
-                    :
-                    <UserDashboard />
-            }
+            Dashboard
         </div>
     )
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,91 +1,9 @@
-// importing components
-import { Formik, Form, ErrorMessage, Field } from 'formik';
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import AuthContext from '../../context/auth';
-
-// importing helpers
-import {
-    loginUser,
-    getClientMachineInformation
-} from '../../utils/helper';
-
 // login components
 function Login() {
-    const navigator = useNavigate();
-    const authCtx = useContext(AuthContext);
-
     return (
-        <div className="login container">
-            <h1>
-                Login Page
-            </h1>
-
-            <div className="mb-4">
-                Dont have an account? <Link to="/signup">Signup</Link>
-            </div>
-
-            <Formik
-                initialValues={{ email: '', password: '' }}
-
-                validate={values => {
-                    // TODO: custom validation needed here
-
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-
-                onSubmit={async (values, actions) => {
-                    const res = await loginUser({
-                        email: values.email,
-                        password: values.password,
-                        clientMachineInformation: getClientMachineInformation()
-                    })
-
-                    if (res.error) {
-                        alert('Login failed!');
-                    } else {
-                        // setting value in auth context
-                        authCtx.login(res.data.token);
-
-                        actions.setSubmitting(false);
-
-                        // redirect user to dashboard
-                        navigator('/dashboard', { replace: true })
-                    }
-
-                    // reset the form
-                    actions.resetForm();
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form className="loginForm">
-                        <div className="mb-1">
-                            <Field type="email" name="email" placeholder="Email Address" />
-                            <ErrorMessage name="email" component="div" />
-                        </div>
-
-                        <div className="mb-1">
-                            <Field type="password" name="password" placeholder="Password" />
-                            <ErrorMessage name="password" component="div" />
-                        </div>
-
-                        <div className="mt-4">
-                            <button type="submit" disabled={isSubmitting}>
-                                Login
-                            </button>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-        </div >
+        <div>
+            Login
+        </div>
     )
 }
 
