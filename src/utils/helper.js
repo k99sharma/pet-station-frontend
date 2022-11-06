d90c6e435c124ed8e02cf86d804104549e5863f0
ghost32 <kalash.strt@gmail.com>
2022-11-06 14:42:32 +0530

    add: authentication is added
     added login and signup pages
    
     - helper functions are created for login and signup request
     - formik is used to create forms
     - axios is used for HTTP requests
     - Yup is added for schema validation in forms

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -7,14 +7,52 @@ import CONFIGS from "../config/config";
 
 // function to make login request
 export async function loginUser(email, password) {
-    const response = await axios.post(`${CONFIGS.API_URL}/login`, {
+    let result;
+
+    await axios.post(`${CONFIGS.API_URL}/login`, {
         email,
         password
     })
+        .then(res => { result = res.data })
+        .catch(err => {
+            if (err.response) {
+                result = err.response.data;
+            } else if (err.request) {
+                console.log(err.request);
+            } else {
+                console.log('Error', err.message);
+            }
+        })
 
-    return response.data;
+    return result;
 }
 
-export function signupUser() {
-    return "Signup"
+export async function signupUser(data) {
+    const payload = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        gender: data.gender,
+        email: data.email,
+        password: data.password,
+        street: data.street,
+        region: data.region,
+        country: data.country,
+        postalZip: data.postalZip
+    }
+
+    let result;
+
+    await axios.post(`${CONFIGS.API_URL}/signup`, payload)
+        .then(res => { result = res.data })
+        .catch(err => {
+            if (err.response) {
+                result = err.response.data;
+            } else if (err.request) {
+                console.log(err.request);
+            } else {
+                console.log('Error', (err.message))
+            }
+        })
+
+    return result;
 }
