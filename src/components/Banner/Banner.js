868e39ccdebea0f522b7139ca450e98a4d90f23c
ghost32 <kalash.strt@gmail.com>
2022-12-12 00:03:57 +0530

    add: designed home page
     todo: make design responsive

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,18 @@
+// Banner component
+export default function Banner() {
+    return (
+        <div className="banner inline-flex items-center p-2">
+            <div className="banner__logo">
+                <img
+                    src='/assets/paw1.png'
+                    className="w-8 h-8"
+                    alt="logo"
+                />
+            </div>
+
+            <div className="banner__name text-lg mx-4">
+                Pet Station
+            </div>
+        </div>
+    )
+}               
