5b6ff1df3b933a972966f57f315a72df47b6f9ff
ghost32 <kalash.strt@gmail.com>
2022-12-17 22:08:04 +0530

    change: changed banner icon

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,20 +1,19 @@
 // importing components
 import { Link } from 'react-router-dom';
+import { GiBoatHorizon } from 'react-icons/gi';
 
 // Banner component
 export default function Banner() {
     return (
         <Link to="/">
             <div className="banner inline-flex items-center p-2">
-                <div className="banner__logo">
-                    <img
-                        src='/assets/paw1.png'
+                <div className="banner-logo">
+                    <GiBoatHorizon
                         className="w-8 h-8"
-                        alt="logo"
                     />
                 </div>
 
-                <div className="banner__name text-lg mx-4">
+                <div className="banner-name text-lg mx-4">
                     Pet Station
                 </div>
             </div>
