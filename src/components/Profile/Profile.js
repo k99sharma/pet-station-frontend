7912b165c9fc9fd0018692ad6fc7bea08229f016
ghost32 <kalash.strt@gmail.com>
2022-12-17 23:45:16 +0530

    add: board layout
     - changed layout of board
     - added background color of body
     - added new class box-container for board

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,24 +9,26 @@ export default function Profile(props) {
     const { authCtx } = props;
 
     return (
-        <div className="profile p-5">
-            <div className="profile-header mb-5">
-                <Heading
-                    sticker="/assets/profile1.png"
-                    heading="Welcome Back!"
-                    subheading="Where love and pets meet"
-                />
+        <div className="profile flex flex-col justify-center items-center">
+            <div className="profile-content box-container rounded-2xl md:w-4/5">
+                <div className="profile-header mb-5">
+                    <Heading
+                        sticker="/assets/profile1.png"
+                        heading="Welcome Back!"
+                        subheading="Where love and pets meet"
+                    />
+                </div>
+
+                <div className="profile-petSection">
+                    <PetSection token={authCtx.token} />
+                </div>
+
+                <div className="profile-adoptionHistory">
+                    <AdoptionHistory token={authCtx.token} />
+                </div>
             </div>
 
-            <div className="profile-petSection">
-                <PetSection token={authCtx.token} />
-            </div>
-
-            <div className="profile-adoptionHistory">
-                <AdoptionHistory token={authCtx.token} />
-            </div>
-
-            <div className="profile-copyright font-lighter text-xs text-center text-neutral-800 mt-10">
+            <div className="profile-copyright font-lighter text-xs text-center text-neutral-800 my-10">
                 {
                     `Copyright © ${new Date().getFullYear()} Pet Station. All rights reserved.`
                 }
